395837206799f333aeb69dbc7ba336d1de3a3edf
Tomek Wilczak <tomek.wilczak@gmail.com>
2020-09-23 07:56:23 +0200

    Changed positioning with flex

--- js/countdown.js
+++ js/countdown.js
@@ -16,170 +16,174 @@
  * increment time must be divided by ten.
  */
 
-var countdown = new (function() {
-    var $countdown,
-        $form, // Form used to change the countdown time
-        incrementTime = 70,
-        currentTime = 360000,
-        updateTimer = function() {
-            $countdown.html(formatTime(currentTime));
-            if (currentTime == 0) {
-                countdown.Timer.stop();
-                timerComplete();
-                countdown.resetCountdown();
-                return;
-            }
-            currentTime -= incrementTime / 10;
-            if (currentTime < 0) currentTime = 0;
-        },
-        timerComplete = function() {
-		gameover.playclip();
-            	alert('Game over!');
-        },
-        init = function() {
-            $countdown = $('.countdown-timer');
-            countdown.Timer = $.timer(updateTimer, incrementTime, false);
-            $form = $('#cpanel');
-            $form.bind('submit', function() {
-                countdown.resetCountdown();
-                return false;
-            });
-        };
-    this.resetCountdown = function() {
-        var newTime = parseInt($form.find('input[type=text]').val()) * 100;
-        if (newTime > 0) {currentTime = newTime;}
-        this.Timer.stop().once();
+var countdown = new (function () {
+  var $countdown,
+    $form, // Form used to change the countdown time
+    incrementTime = 70,
+    currentTime = 180000,
+    updateTimer = function () {
+      $countdown.html(formatTime(currentTime));
+      if (currentTime == 0) {
+        countdown.Timer.stop();
+        timerComplete();
+        countdown.resetCountdown();
+        return;
+      }
+      currentTime -= incrementTime / 10;
+      if (currentTime < 0) currentTime = 0;
+    },
+    timerComplete = function () {
+      gameover.playclip();
+      alert("Game over!");
+    },
+    init = function () {
+      $countdown = $(".countdown-timer");
+      countdown.Timer = $.timer(updateTimer, incrementTime, false);
+      $form = $("#cpanel");
+      $form.bind("submit", function () {
+        countdown.resetCountdown();
+        return false;
+      });
     };
-    $(init);
-});
+  this.resetCountdown = function () {
+    var newTime = parseInt($form.find("input[type=text]").val()) * 100;
+    if (newTime > 0) {
+      currentTime = newTime;
+    }
+    this.Timer.stop().once();
+  };
+  $(init);
+})();
 
 // Common functions
 function pad(number, length) {
-    var str = '' + number;
-    while (str.length < length) {str = '0' + str;}
-    return str;
+  var str = "" + number;
+  while (str.length < length) {
+    str = "0" + str;
+  }
+  return str;
 }
 function formatTime(time) {
-    var min = parseInt(time / 6000),
-        sec = parseInt(time / 100) - (min * 60),
-        hundredths = pad(time - (sec * 100) - (min * 6000), 2);
-    return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
-}	
+  var min = parseInt(time / 6000),
+    sec = parseInt(time / 100) - min * 60,
+    hundredths = pad(time - sec * 100 - min * 6000, 2);
+  return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
+}
 
 //Skrypt audio
-var audiotypes={
-        "mp3": "audio/mpeg",
-        "mp4": "audio/mp4",
-        "ogg": "audio/ogg",
-        "wav": "audio/wav"
+var audiotypes = {
+  mp3: "audio/mpeg",
+  mp4: "audio/mp4",
+  ogg: "audio/ogg",
+  wav: "audio/wav",
+};
+function ss_soundbits(sound) {
+  var audio_element = document.createElement("audio");
+  if (audio_element.canPlayType) {
+    for (var i = 0; i < arguments.length; i++) {
+      var source_element = document.createElement("source");
+      source_element.setAttribute("src", arguments[i]);
+      if (arguments[i].match(/\.(\w+)$/i))
+        source_element.setAttribute("type", audiotypes[RegExp.$1]);
+      audio_element.appendChild(source_element);
     }
-function ss_soundbits(sound){
-        var audio_element = document.createElement('audio')
-        if (audio_element.canPlayType){
-            for (var i=0; i<arguments.length; i++){
-                var source_element = document.createElement('source')
-                source_element.setAttribute('src', arguments[i])
-                if (arguments[i].match(/\.(\w+)$/i))
-                    source_element.setAttribute('type', audiotypes[RegExp.$1])
-                audio_element.appendChild(source_element)
-            }
-            audio_element.load()
-            audio_element.playclip=function(){
-                audio_element.pause()
-                audio_element.currentTime=0
-                audio_element.play()
-            }
-            return audio_element
-        }
-    }
-	
-	var winners  = ss_soundbits('audio/winners.mp3');
-   	var hint  = ss_soundbits('audio/notification.mp3');
-	var gameover  = ss_soundbits('audio/game-over.mp3');
-	var depesza  = ss_soundbits('audio/depesza.mp3');
-	var soundtrack  = ss_soundbits('audio/soundtrack.mp3');
+    audio_element.load();
+    audio_element.playclip = function () {
+      audio_element.pause();
+      audio_element.currentTime = 0;
+      audio_element.play();
+    };
+    return audio_element;
+  }
+}
+
+var winners = ss_soundbits("audio/winners.mp3");
+var hint = ss_soundbits("audio/notification.mp3");
+var gameover = ss_soundbits("audio/game-over.mp3");
+var depesza = ss_soundbits("audio/depesza.mp3");
+var soundtrack = ss_soundbits("audio/soundtrack.mp3");
 
 // MOJE ZMIANY (odtwarzanie audio, buttony, show/hide itp.)
 
 //Start odliczania czasu + start soundtrack
-var start = function() {
-	countdown.Timer.toggle();
-	soundtrack.play();
+var start = function () {
+  countdown.Timer.toggle();
+  soundtrack.play();
 };
 
 //Winners: muzyka + obraz "Mission Accomplished"
-var winners_play = function() {
-   	winners.playclip(); 
-	countdown.Timer.stop();
-	depesza.pause();
-	soundtrack.pause();
-	$('#winnersdiv').slideDown('slow');
+var winners_play = function () {
+  winners.playclip();
+  countdown.Timer.stop();
+  depesza.pause();
+  soundtrack.pause();
+  $("#winnersdiv").slideDown("slow");
 };
 
 //Button_music odtwarzanie muzyki (start/stop)
 var action = 1;
-var music = function() {
-	 if ( action == 1 ) {
-        soundtrack.pause();
-        action = 2;
-    } else {
-        soundtrack.play();
-        action = 1;
-    }
+var music = function () {
+  if (action == 1) {
+    soundtrack.pause();
+    action = 2;
+  } else {
+    soundtrack.play();
+    action = 1;
+  }
 };
 
 //Button_mute zatrzymywanie wszystkich dźwieków
-var mute = function() {
-	depesza.pause();
-	soundtrack.pause();
-	winners.pause(); 
-	action = 2;
+var mute = function () {
+  depesza.pause();
+  soundtrack.pause();
+  winners.pause();
+  action = 2;
 };
 
 //Button_depesza odtwarzanie depeszy radiowej ("Sierra Alpha [...]")
-var depesza_play = function() {
-	depesza.playclip();
-	action = 2;
+var depesza_play = function () {
+  depesza.playclip();
+  action = 2;
 };
 
 //Odkrywanie/chowanie control panelu przyciskami
 
-$(document).ready(function() {
-    $('#logo').click(function () {
-        $('#cpanel').toggle();
-    });
-    $('#button_play').click(function () {
-        $('#cpanel').toggle();
-    });
-    $('#button_winners').click(function () {
-        $('#cpanel').toggle();
-    });
+$(document).ready(function () {
+  $("#logo").click(function () {
+    $("#cpanel").toggle();
+  });
+  $("#button_play").click(function () {
+    $("#cpanel").toggle();
+  });
+  $("#button_winners").click(function () {
+    $("#cpanel").toggle();
+  });
 });
 
 //Dodawanie wskazówek przyciskiem "Add"
-$(document).ready(function() {
-    $("#button_add").click(function (){
-        var toAdd = $("input[name=message]").val();
-        $('#messages').append('<div class="item">' + toAdd + '</div>');
-	$("input[name=message]").val('');
-	hint.playclip(); 
-	$('#cpanel').hide();
-     });
+$(document).ready(function () {
+  $("#button_add").click(function () {
+    var toAdd = $("input[name=message]").val();
+    $("#messages").append('<div class="item">' + toAdd + "</div>");
+    $("input[name=message]").val("");
+    hint.playclip();
+    $("#cpanel").hide();
+  });
 
-//Dodawanie wskazówek klawiszem "enter"
-$('input[name=message]').on('keypress', function(e) {
-	var code = e.keyCode || e.which;
-	if(code==13) {
-		var toAdd = $('input[name=message]').val();
-		$('#messages').append('<div class="item">' + toAdd + '</div>');
-		hint.playclip();
-		$('input[name=message]').val('');
-		$('#cpanel').hide();
-	}
-	});
+  //Dodawanie wskazówek klawiszem "enter"
+  $("input[name=message]").on("keypress", function (e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+      var toAdd = $("input[name=message]").val();
+      $("#messages").append('<div class="item">' + toAdd + "</div>");
+      hint.playclip();
+      $("input[name=message]").val("");
+      $("#cpanel").hide();
+    }
+  });
 
-//Usuwanie linijek ze wskazówkami poprzez kliknięcie
-$(document).on('click','.item', function() {
-   $(this).remove();
-    })
-})
+  //Usuwanie linijek ze wskazówkami poprzez kliknięcie
+  $(document).on("click", ".item", function () {
+    $(this).remove();
+  });
+});
